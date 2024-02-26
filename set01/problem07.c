a1cf013d74beba0b5742c01851605d48bf3b4978
Chandanadm <chandanad.bsc23@rvu.edu.in>
2024-02-26 10:37:15 +0530

    test

--- set01/problem07.c
+++ set01/problem07.c
@@ -32,5 +32,5 @@ int sum_n_nos(int n)
  
 void output(int n, int sum)  
 {
-    printf("The sum of natural numbers from 1 to %d is %d\n", n , sum);
+    printf("The sum of natural numbers from 1 to %d is %d ", n , sum);
 }
