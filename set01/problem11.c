115496eab0f0c52519762ccc6022051d3f047fd9
Chandanadm <chandanad.bsc23@rvu.edu.in>
2024-02-27 14:18:24 +0530

    test

--- set01/problem11.c
+++ set01/problem11.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-struct _complex {float real,imaginary; };
+struct _complex {float real,img; };
 typedef struct _complex Complex; 
 int get_n(); 
 Complex input_complex(); 
@@ -31,8 +31,8 @@ Complex input_complex()
     Complex c;
     printf("Enter real part: ");
     scanf("%f", &c.real);
-    printf("Enter imaginary part: ");
-    scanf("%f", &c.imaginary);
+    printf("Enter img part: ");
+    scanf("%f", &c.img);
     return c;
 }
 void input_n_complex(int n, Complex c[n])
@@ -48,7 +48,7 @@ Complex add(Complex a, Complex b)
 {
     Complex sum;
     sum.real = a.real + b.real;
-    sum.imaginary = a.imaginary + b.imaginary;
+    sum.img = a.img + b.img;
     return sum;
 }
 Complex add_n_complex(int n, Complex c[n])
@@ -65,7 +65,7 @@ void output(int n, Complex c[n], Complex result)
     printf("Complex numbers entered:\n");
     for (int i = 0; i < n; i++)
     {
-        printf("%f + %fi\n", c[i].real, c[i].imaginary);
+        printf("%f + %fi\n", c[i].real, c[i].img);
     }
-    printf("Sum of complex numbers: %f + %fi\n", result.real, result.imaginary);
+    printf("Sum of complex numbers: %f + %fi\n", result.real, result.img);
 }
