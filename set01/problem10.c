728285ff5805033bfa3ac95fa44f23d2eba528a7
Chandanadm <chandanad.bsc23@rvu.edu.in>
2024-03-01 14:40:35 +0530

    test

--- set01/problem10.c
+++ set01/problem10.c
@@ -1,65 +1,46 @@
 #include <stdio.h>
+#include <string.h>
+void input_two_strings(char *string1, char *string2);
+int stringcompare(char *string1, char *string2);
+void output(char *string1, char *string2, int result);
 
-int input_array_size();
-void input_array(int n, int a[n]);
-int sum_composite_numbers(int n, int a[n]);
-void output(int sum);
-
-int main() {
-    int size, sum;
-
-    size = input_array_size();
-    
-    int array[size];
-    input_array(size, array);
-
-    sum = sum_composite_numbers(size, array);
-
-    output(sum);
-
+int main()
+{
+    int result;
+    char string1[100], string2[100];
+    input_two_strings(string1, string2);
+    result=stringcompare(string1, string2);
+    printf("result is %d\n", result);
+    output(string1, string2, result);
     return 0;
+   
+}
+void input_two_strings(char *string1, char *string2)
+{
+  printf("Enter the first string\n");
+  scanf("%s[^\n]", string1);
+  printf("Enter the second string\n");
+  scanf("%s[^\n]", string2);
 }
 
-int input_array_size() {
-    int n;
-    printf("Enter the size of the array: ");
-    scanf("%d", &n);
-    return n;
+int stringcompare(char *string1, char *string2)
+{
+  int i=0;
+  for(i=0;string1[i]!='\0' && string2[i]!='\0' && string1[i]==string2[i];i++);
+
+  if(string1[i]==string2[i])
+    return 0;
+  if(string1[i]>string2[i])
+    return 1;
+  else return 2;
 }
 
-void input_array(int n, int a[n]) {
-    printf("Enter %d numbers separated by space: ", n);
-    for (int i = 0; i < n; ++i) {
-        scanf("%d", &a[i]);
-    }
-}
-
-int is_composite(int num) {
-    if (num < 4) {
-        return 0; 
-    }
-
-    for (int i = 2; i <= num / 2; ++i) {
-        if (num % i == 0) {
-            return 1; 
-        }
-    }
-
-    return 0; 
-}
-
-int sum_composite_numbers(int n, int a[n]) {
-    int sum = 0;
-
-    for (int i = 0; i < n; ++i) {
-        if (is_composite(a[i])) {
-            sum += a[i];
-        }
-    }
-
-    return sum;
-}
-
-void output(int sum) {
-    printf("The sum of composite numbers in the array is %d\n", sum);
+void output(char *string1, char *string2, int result)
+{
+  if(result==0)
+    printf("%s is equal to %s\n", string1, string2);
+  else if(result==1)
+    printf("%s is greater than %s\n", string1, string2);
+  else if(result==2)
+    printf("%s is greater than %s\n", string2, string1);
 }
